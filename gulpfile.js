63e3b2699a66b00c3a29f1546fdd6de602a82ff8
Russell Long <russelllongj@gmail.com>
2018-08-06 20:18:07 +1000

    Added gulp dependency and basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+
+gulp.task('default', function() {
+	console.log("Your only option right now is `gulp serve`");
+})
+
+gulp.task('serve', function() {
+	console.log("serving the application");
+});
