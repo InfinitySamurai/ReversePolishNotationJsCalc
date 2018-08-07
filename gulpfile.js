1cf84134b8928047ed23b489462769f72bac4091
Russell Long <russelllongj@gmail.com>
2018-08-07 12:56:41 +1000

    Added gulp integration and updated some tslint rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp')
+var tslint = require('gulp-tslint')
 
-gulp.task('default', function() {
-	console.log("Your only option right now is `gulp serve`");
+gulp.task('default', () => {
+  console.log("Your only option right now is `gulp serve`");
+})
+
+gulp.task('tslint', () => {
+  gulp.src('src/**/*')
+    .pipe(tslint({
+      formatter: "verbose"
+    }))
+    .pipe(tslint.report({
+      allowWarnings: true
+    }))
 })
 
 gulp.task('serve', function() {
-	console.log("serving the application");
+  console.log("serving the application");
 });
